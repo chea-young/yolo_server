fd0a82ca1b60f2cdec3d1d091573cd5823449cbf
chea-young <63410509+chea-young@users.noreply.github.com>
2020-11-06 23:02:30 +0900

    MODIFY 이미지 삭제되는 것까지 확인

--- tutorial/RID/RID/select_image.py
+++ tutorial/RID/RID/select_image.py
@@ -4,19 +4,28 @@ def select(video_path):
     image_path='./output_image/'
     saved_image=image_path+video_path+'/'
     image_list = os.listdir(saved_image)
-    frame_num = 0
-    delte_num = 0
+    frame_num = int(image_list[0][:-4])
+    delte_num = frame_num
+
     for i in range(len(image_list)):
         frame_name = int(image_list[i][:-4])
-        if(frame_num+1 != frame_name and frame_name != 0):
-            if(frame_num != 0):
-                for j in range(delte_num, frame_num+1):
-                    delte_image(saved_image, j)
+        if(frame_num != frame_name):
+            print(frame_num,frame_name)
+            for j in range(delte_num+1, frame_num):
+                delte_image(saved_image, j)
+                #delte_num은 알람을 보내고 삭제하기
+                #여기 알림보내는 코드 부르기
             frame_num=frame_name
             delte_num = frame_name
+        frame_num+= 1
+    print(delte_num, frame_num)
+    for j in range(delte_num+1, frame_num):
+        delte_image(saved_image, j)
+        #delte_num은 알람을 보내고 삭제하기
+        #여기 알림보내는 코드 부르기
 
 def delte_image(image_path, image_name):
-    image_file = image_path+image_name+'.png'
+    image_file = image_path+str(image_name)+'.png'
     if os.path.isfile(image_file):
         os.remove(image_file)
     
